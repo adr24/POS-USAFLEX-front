853ed91248c72c76dc668c18010ec266966f636d
Ale <duartemattias4@gmail.com>
2024-03-08 21:39:45 -0400

    initial config

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import { Button } from '@nextui-org/react'
+
+
+export default function App() {
+
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+
+
+      <Button>Button</Button>
+    </div>
+
+  )
+}
