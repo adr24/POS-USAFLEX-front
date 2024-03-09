f80ae5593396c9219e0a7a9e0bf000a2a7aba0e9
Ale <duartemattias4@gmail.com>
2024-03-08 23:00:17 -0400

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
 
+import { useState } from 'react';
 import { Button } from '@nextui-org/react'
 
 
 export default function App() {
 
+  const [ numero, setNumero ] = useState(0);
+
+
+  const increment = () => {
+    
+    setNumero( numero + 1 )
+    
+  }
+
+  
+  const decrement = () => {
+    
+    setNumero( numero - 1 )
+    
+  }
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
+    <div className='min-h-screen flex justify-center items-center flex-col'>
+      <h1 className="text-3xl font-bold">
+        Contador
       </h1>
 
+      <p className='text-5xl my-3'>{ numero }</p>
 
-
-      <Button>Button</Button>
+      <div className='flex gap-4 items-center'>
+        <Button onClick={increment} >Sumar</Button>
+        <Button onClick={() => setNumero(0)} >Reiniciar</Button>
+        <Button onClick={decrement} >Restar</Button>
+      </div>
     </div>
 
   )
